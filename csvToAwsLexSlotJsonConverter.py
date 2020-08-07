821bce2d45d104a875a38d309eb7c8816623203f
Angel J Faraldo <angeljfaraldo@gmail.com>
2020-08-07 13:39:10 -0400

    Add files via upload

--- csvToAwsLexSlotJsonConverter.py
+++ csvToAwsLexSlotJsonConverter.py
@@ -1,12 +1,11 @@
-### to execute this python script the CLI command must be formatted as follows
-### python csvFileNameToBeConverted(&location if necessary) TitleOfFile titleOfSlot 
+### to execute this python script the CLI command must be formatted as follows:
+### python nameOfThisScript.py csvFileNameToBeConverted.csv(&fileLocation if necessary) titleOfSlot titleOfOutputJsonFile
 ### EXAMPLE:
-### $ python botCsv2JsonConverter.py test.csv outputTesting.json newSlotName
+### $ python csvToAwsLexSlotJsonConverter.py csvToConvert.csv newSlotName outputConvertedJsonFile.json
 
 import csv
 import json
 import sys
-
 
 
 data = {
@@ -16,7 +15,7 @@ data = {
     "importFormat": "JSON"
   },
   "resource": {
-    "name": sys.argv[3],
+    "name": sys.argv[2],
     "version": "1",
     "enumerationValues": [],
    "valueSelectionStrategy": "TOP_RESOLUTION"
@@ -26,18 +25,19 @@ data = {
 with open(sys.argv[1]) as csvfile:
      reader = csv.reader(csvfile)
      csv_data =  ([{"value": row[0], "synonyms": (row)} for row in reader])
+     csvfile.close()
 
-
-with open(sys.argv[2], 'w') as jsonfile:
+with open(sys.argv[3], 'w') as jsonfile:
      jsonfile.write(json.dumps(data, indent=1))
      jsonfile.close()
      
-with open(sys.argv[2], 'r+') as jsonfiles:
-    data_2 = json.load(jsonfiles)
+with open(sys.argv[3], 'r+') as jsonf:
+    data_2 = json.load(jsonf)
     data_2['resource']['enumerationValues'] = csv_data
-    jsonfiles.seek(0)        # <--- should reset file position to the beginning.
-    json.dump(data_2, jsonfiles, indent=1)
+    jsonf.seek(0)        # <--- should reset file position to the beginning.
+    json.dump(data_2, jsonf, indent=1)
+    jsonfile.close()
 
 # show result
-with open(sys.argv[2], 'r') as jsonfile:
+with open(sys.argv[3], 'r') as jsonfile:
     print(json.dumps(json.load(jsonfile), indent=1))
